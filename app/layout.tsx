02f0de6b5d0abb7ec69906660a95bbb9c8954aa3
wotinnovation <media@wot.ae>
2026-05-22 11:09:51 +0400

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Barlow, Barlow_Condensed } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const barlow = Barlow({
@@ -30,30 +29,6 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${barlow.variable} ${barlowCondensed.variable} text-lg`}>
         {children}
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=832707186563230&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '832707186563230');
-            fbq('track', 'PageView');
-          `}
-        </Script>
       </body>
     </html>
   );
